fe25d09189b899a5bd7df705a18e684061472836
Humberto Domínguez <humbertodelac13@gmail.com>
2024-03-24 21:09:24 -0600

    cascaron de app creado

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-
-`
-
-
+document.querySelector("#app").innerHTML = `
+<h1 class="main__title">Consumiendo API de Rick and Morty</h1>
+<button class="main__button" id="main__button">Agregar personaje</button>
+<div class="main__characters" id="main__characters">
+  
+</div>
+`;
